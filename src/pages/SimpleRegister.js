ed08b844a1dc9bcfc0cc1c8b10cd9f96d1d64a14
NurKevser <nurkevserkadiroglu@gmail.com>
2021-09-16 21:35:54 +0300

    if input is empty, get the error message

--- src/pages/SimpleRegister.js
+++ src/pages/SimpleRegister.js
@@ -10,27 +10,53 @@ function SimpleRegister() {
           name: "",
           email: "",
           userName: "",
+        },
+        onSubmit:values=>{
+            console.log("values: ", values);
+        },
+        validate: values => {
+            let errors={}
+            if (!values.name){
+                errors.name="Required.Please fill this area"
+            }
+            if (!values.email){
+                errors.email="Required.Please fill this area"
+            }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';}
+            if (!values.userName){
+                errors.userName="Required.Please fill this area"
+            }
+
+
+            return errors
         }
       })
 
-    //   console.log("formik: ", formik);
-
+    //   console.log("formik: ", formik.values);
+    //  console.log("formik touch: ",formik.touched);
     return (
         <div className="container">
             <h1>Simple Form</h1>
             
-            <form className="formStyle">
+            <form className="formStyle" onSubmit={formik.handleSubmit} >
                 <label  htmlFor="name">Name</label>
                 <input type="text" id="name" name="name"
-                onChange={formik.handleChange} value={formik.values.name} ></input>
+                onChange={formik.handleChange} 
+                value={formik.values.name}
+                onBlur={formik.handleBlur}></input>
+                {formik.touched.name && formik.errors.name ? <div className="errorStyle">{formik.errors.name}</div> : null}
+
                 
                 <label  htmlFor="email">Email</label>
                 <input type="text" id="email" name="email"
                 onChange={formik.handleChange} value={formik.values.email}></input>
+                {formik.touched.email && formik.errors.email ? <div className="errorStyle">{formik.errors.email}</div> : null}
+                
 
                 <label htmlFor="userName">User name</label>
                 <input type="text" id="userName" name="userName"
                 onChange={formik.handleChange} value={formik.values.userName}></input>
+                {formik.touched.userName && formik.errors.userName ? <div className="errorStyle">{formik.errors.userName}</div> : null}
 
                 <button>Submit</button>
             </form>
